de11d2c52e00be57b92e5e393c180ad16739ee02
Aymeric Goube <aymericgoube@hotmail.fr>
2022-08-29 14:47:49 +0200

    adding attendees model

--- models/Event.model.js
+++ models/Event.model.js
@@ -1,37 +1,36 @@
-const { Schema, model } = require('mongoose');
-const User = require('./User.model');
+const { Schema, model } = require("mongoose");
 
-const eventSchema = new Schema (
-    {
-        title: {
-            type: Schema.Types.String,
-            required: true,
-            maxlength: 50,
-        },
-        date: {
-            type: Schema.Types.Date,
-            required: true,
-        },
-        city: {
-            type: Schema.Types.String,
-            required: true,
-            // temp enum while Google Places API not implemented
-            enum: ['Lille', 'Paris', 'Lyon', 'Marseille', 'Bordeaux', 'Nantes'],
-        },
-        promoter: {
-            type: Schema.Types.ObjectId,
-            ref: 'User',
-        },
-        attendees: {
-            type: [Schema.Types.ObjectId],
-            ref: 'User'
-        },
+const eventSchema = new Schema(
+  {
+    title: {
+      type: Schema.Types.String,
+      required: true,
+      maxlength: 50,
     },
-    {
-        timestamps: true,
-    }
+    date: {
+      type: Schema.Types.Date,
+      required: true,
+    },
+    city: {
+      type: Schema.Types.String,
+      required: true,
+      // temp enum while Google Places API not implemented
+      enum: ["Lille", "Paris", "Lyon", "Marseille", "Bordeaux", "Nantes"],
+    },
+    promoter: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    attendees: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Event = model('Event', eventSchema);
+const Event = model("Event", eventSchema);
 
 module.exports = Event;
--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,9 +1,12 @@
-const router = require('express').Router();
-const User = require('../models/User.model');
-const Event = require('../models/Event.model');
-const { isAuthenticated, isAdminOrPromoter } = require('../middleware/middleware')
+const router = require("express").Router();
+const User = require("../models/User.model");
+const Event = require("../models/Event.model");
+const {
+  isAuthenticated,
+  isAdminOrPromoter,
+} = require("../middleware/middleware");
 
-/** 
+/**
  * all routes are prefix by /api/event
  */
 
@@ -25,46 +28,39 @@ router.post("/newEvent", isAuthenticated, async (req, res, next) => {
 });
 
 // event calling
-
 router.get("/event-list", async (req, res, next) => {
   const city = req.query.city;
-
-
-router.get('/event-list', async (req, res, next) => {
-    const city = req.query.city;
-    try {
-        if(city) {
-            const cityEvents = await Event.find({ city });
-            return res.status(302).json({ cityEvents });
-        }
-        res.status(302).json(await Event.find());
-    } catch (error) {
-        next(error);
+  try {
+    if (city) {
+      const cityEvents = await Event.find({ city });
+      return res.status(302).json({ cityEvents });
     }
+    res.status(302).json(await Event.find());
+  } catch (error) {
+    next(error);
+  }
 });
 
 // event update
-router.patch('/:id', isAuthenticated, isAdminOrPromoter, async (req, res, next) => {
+router.patch(
+  "/:id",
+  isAuthenticated,
+  isAdminOrPromoter,
+  async (req, res, next) => {
     const { title, date } = req.body;
     try {
-        if (title) {
-            await Event.findByIdAndUpdate(
-                req.params.id,
-                { title },
-                { new: true },
-            )
-        };
-        if (date) {
-            await Event.findByIdAndUpdate(
-                req.params.id,
-                { date },
-                { new: true },
-            )
-        };
-        res.status(202).json(await Event.findById(req.params.id));
-    } catch(error) {
-        next(error);
+      if (title) {
+        await Event.findByIdAndUpdate(req.params.id, { title }, { new: true });
+      }
+      if (date) {
+        await Event.findByIdAndUpdate(req.params.id, { date }, { new: true });
+      }
+      res.status(202).json(await Event.findById(req.params.id));
+    } catch (error) {
+      next(error);
     }
+  }
+);
 
 // delete event
 router.delete("/deleteEvent/:id", isAuthenticated, async (req, res, next) => {
